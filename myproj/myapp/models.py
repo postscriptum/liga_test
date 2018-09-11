637083c50dab56397fd788286150e7cccc03cb78
Dmitriy Semenovskiy <dmitriy.semenovskiy@gmail.com>
2018-09-11 21:51:14 +0300

    create models Source and Document

--- myproj/myapp/models.py
+++ myproj/myapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Source(models.Model):
+    sid = models.SlugField()
+    name = models.CharField(max_length=100)
+    url = models.URLField()
+
+
+class Document(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    url = models.URLField()
+    created = models.DateTimeField()
+    source = models.ForeignKey(Source, on_delete=models.CASCADE)
+    added = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    edit_count = models.PositiveSmallIntegerField(default=0)
